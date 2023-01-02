c402f9d881c2ba0b44fd27e2f8e0650a5596ce27
BIX5_leh <kkh60245@gmail.com>
2023-01-02 16:40:56 +0900

    9. 슬라이드 네비게이터 보이기/숨기기

--- BIX5/JS/settingDashboard.js
+++ BIX5/JS/settingDashboard.js
@@ -4,8 +4,8 @@ var removeSlide = function() {
 }
 
 // 집고 넘어가야 하는 함수 - dashboard의 get함수
-// 이 함수로 대시보드 인스턴스를 얻는다. 해당 인스턴스로 대시보드를 제어할 수 있다.
-// get(id) -- id(type string, 생성된 대시보드의 id)
+ // 이 함수로 대시보드 인스턴스를 얻는다. 해당 인스턴스로 대시보드를 제어할 수 있다.
+ // get(id) -- id(type string, 생성된 대시보드의 id)
 var movePrev = function() {
     // 이전 슬라이드로 이동
     BIX5.dashboard.get('dashboard1').movePrev();
@@ -31,6 +31,13 @@ var moveSlideAt = function() {
     var idx = document.getElementById("slideIdx").value*1;
     // 해당 인덱스 슬라이드로 이동한다.
     BIX5.dashboard.get('dashboard1').setSelectedIndex(idx);
+}
+
+
+var showSlideNavigator = function(chk) {
+    //dashboard의 showSlideNavigator 함수에 인수로 true를 넘기면 하단 슬라이드 네비게이터가 보인다.
+     //함수 안에 boolen 값을 넣는다.
+    BIX5.dashboard.get("dashboard1").showSlideNavigator(chk);
 }
 
 
@@ -71,12 +78,13 @@ BIX5.dashboard.create("dashboard1", "dashboardHolder", function(dashboard){
     // borderWidth : 보더의 굵기를 설정합니다.
     var slides =  [
         {slideName:"slide1",
-        widgets:[{id:"title",x:10,y:0,width:200,height:30, contentUrl :"./Widgets/Html/title.html",type:"html",title:"Untitled"}
-        ,{id:"iframe",x:10,y:30,width:370,height:500, layoutType:"window", type:"iframe",title:"header title", componentOptions:{url:"http://www.bixsoft.net"}, options:{"border":true,"borderColor":"#ccc","borderWidth":"1px"}}
-        ,{id:"clock",x:590,y:0,width:250,height:30, contentUrl:"",type:"clock",title:"clock", options:{border:false}}
-        ,{id:"Dash_Lines",x:390,y:30,width:450,height:160, contentUrl:"./Widgets/Chart/Dash_Lines.html",type:"chart",title:"Dash_Lines",options:{"border":true,"borderColor":"#EEEEEE","borderWidth":"1px"}}
-        ,{id:"Plot",x:390,y:200,width:450,height:160, contentUrl:"./Widgets/Chart/Plot_2D_Ex.html",type:"chart",title:"Plot_2D_Ex",options:{"border":true,"borderColor":"#EEEEEE","borderWidth":"1px"}}
-        ,{id:"Column_2D",x:390,y:370,width:450,height:160, contentUrl:"./Widgets/Chart/Column_2D.html",type:"chart",title:"Column_2D",options:{"border":true,"borderColor":"#EEEEEE","borderWidth":"1px"}}
+        widgets:[
+            {id:"title",x:10,y:0,width:200,height:30, contentUrl :"./Widgets/Html/title.html",type:"html",title:"Untitled"}
+            ,{id:"iframe",x:10,y:30,width:370,height:500, layoutType:"window", type:"iframe",title:"header title", componentOptions:{url:"http://www.bixsoft.net"}, options:{"border":true,"borderColor":"#ccc","borderWidth":"1px"}}
+            ,{id:"clock",x:590,y:0,width:250,height:30, contentUrl:"",type:"clock",title:"clock", options:{border:false}}
+            ,{id:"Dash_Lines",x:390,y:30,width:450,height:160, contentUrl:"./Widgets/Chart/Dash_Lines.html",type:"chart",title:"Dash_Lines",options:{"border":true,"borderColor":"#EEEEEE","borderWidth":"1px"}}
+            ,{id:"Plot",x:390,y:200,width:450,height:160, contentUrl:"./Widgets/Chart/Plot_2D_Ex.html",type:"chart",title:"Plot_2D_Ex",options:{"border":true,"borderColor":"#EEEEEE","borderWidth":"1px"}}
+            ,{id:"Column_2D",x:390,y:370,width:450,height:160, contentUrl:"./Widgets/Chart/Column_2D.html",type:"chart",title:"Column_2D",options:{"border":true,"borderColor":"#EEEEEE","borderWidth":"1px"}}
         ]}
     ];
     
@@ -87,13 +95,13 @@ BIX5.dashboard.create("dashboard1", "dashboardHolder", function(dashboard){
     dashboard.setSlides(slides);
     
     //dashboard의 addSlide함수를 이용하여 슬라이드를 추가할 수 있다.
-    //함수 안에 {} object를 집어 넣는다.
+     //함수 안에 {} object를 집어 넣는다.
     dashboard.addSlide({
         slideName:"slide2",
         widgets:[{x:390, y:260, width:170, height:40, contentHtml:"<h1>slide2</h1>", type:"html"}]
     });
     //dashboard의 addSlildes함수를 이용하여 여러 슬라이드를 추가할 수 있다.
-    //함수 안에 [] array를 집어 넣는다.
+     //함수 안에 [] array를 집어 넣는다.
     dashboard.addSlides([
         {
             slideName:"slide4",
@@ -105,7 +113,7 @@ BIX5.dashboard.create("dashboard1", "dashboardHolder", function(dashboard){
         }
     ]);
     //dashboard의 addSlideAt함수를 이용하여 슬라이드를 "특정" 인덱스에 추가할 수 있다.
-    //addSlide와 비슷하지만, 마지막에 index(type number)을 추가하여 특정 위치를 선정할 수 있다.
+     //addSlide와 비슷하지만, 마지막에 index(type number)을 추가하여 특정 위치를 선정할 수 있다.
     dashboard.setSlideAt({
         slideName:"slide3",
         widgets:[{x:390, y:260, width:170, height:40, contentHtml:"<h1>slide3</h1>", type:"html"}]
@@ -126,14 +134,19 @@ BIX5.dashboard.create("dashboard1", "dashboardHolder", function(dashboard){
         {label:"blank", type:"blank", options: {"border":true,"borderColor":"#a6a6a6","borderWidth":"1px"}}
     ];
     //dashboard의 setWidgetPool 함수에 인수로 위젯 정보가 담긴 array을 넘겨 사이드바의 위젯리스트에 위젯들을 설정할 수 있다.
-    //함수 안에 [] array를 집어 넣는다.
+     //함수 안에 [] array를 집어 넣는다.
     dashboard.setWidgetPool(items);
  
     //dashboard의 setEnabled 함수에 인수로 true를 넘기면 위젯의 이동, 리사이징, 최대화(위젯이 widow 타입인 경우), 닫기버튼이 활성화되고 
-    //false를 넘기면 위젯의 이동, 리사이징, 최대화(위젯이 widow 타입인 경우), 닫기버튼이 비활성화 됩니다. 
+     //false를 넘기면 위젯의 이동, 리사이징, 최대화(위젯이 widow 타입인 경우), 닫기버튼이 비활성화 됩니다. 
     dashboard.setEnabled(false);
     // Q.그럼 dashboard의 create 함수 인자 option의 enabled하고 다른 점은 무엇이지????
     // A. 대시보드 편집 여부를 설정합니다. dashboard.create의 enabled옵션보다 우선시 됩니다. 
      //※ 파라미터 타입이 다른 경우 해당 설정을 실행하지 않습니다.
+
+    //이게 왜 갑자기 추가가 되었는지는 모르겠다. 
+     //위에 추가된 showSlideNavigator함수와 무슨 연관이 있는지는 나중에 차차 알아가야 할 듯.
+     //checked 메서드는 id의 체크 여부를 알아내는 것으로 그 값을 true로 한 것은 아마 초기값을 설정 한 듯 하다.
+    document.getElementById("slideControllerChk").checked = true;
 
 }, "100%","100%", {enabled:true, gridlines:false});
